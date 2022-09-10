16d402e88be965f0d4a4f3c70d3e52353875575c
Anam Zhang <zhanganam@163.com>
2022-09-11 02:20:43 +0800

    test CI

--- src/main/java/top/ctorpad/Main.java
+++ src/main/java/top/ctorpad/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World 2!";
     }
 
     public static void main(String[] args) {
