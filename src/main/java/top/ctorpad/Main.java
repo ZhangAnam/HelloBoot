94ff5e9e8660a281e32859da6ea5df282c5d73ba
Anam Zhang <zhanganam@163.com>
2022-09-11 00:23:52 +0800

    init

--- src/main/java/top/ctorpad/Main.java
+++ src/main/java/top/ctorpad/Main.java
@@ -1,2 +1,21 @@
-package top.ctorpad;public class Main {
+package top.ctorpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Main {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
 }
